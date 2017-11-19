ca85249a1f402e0b1539c9e61267fef00b1936b0
Mina Habib <minahabi@amazon.com>
2017-11-18 16:43:52 -0800

    Fix a typo in the class name

--- src/main/java/com/amazon/asksdk/helloworld/HelloWorldSpeechletRequestStreamHandler.java
+++ src/main/java/com/amazon/asksdk/helloworld/HelloWorldSpeechletRequestStreamHandler.java
@@ -1,3 +1,9 @@
+/**
+ Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+ http://aws.amazon.com/apache2.0/
+ or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
 package com.amazon.asksdk.helloworld;
 
 import java.util.HashSet;
@@ -12,8 +18,7 @@ import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
  * this project using the {@code lambda-compile} Ant task and upload the resulting zip file to power
  * your function.
  */
-
-public class HelloWorldSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+public final class HelloWorldSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
     private static final Set<String> supportedApplicationIds;
     static {
         /*
@@ -25,6 +30,6 @@ public class HelloWorldSpeechletRequestStreamHandler extends SpeechletRequestStr
     }
 
     public HelloWorldSpeechletRequestStreamHandler() {
-        super(new HellowWorldSpeechlet(), supportedApplicationIds);
+        super(new HelloWorldSpeechlet(), supportedApplicationIds);
     }
 }
