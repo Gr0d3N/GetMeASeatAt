c7cae4fdb5f1b62f9dd845e4728edeccd14c8ca6
Mina Habib <minahabi@amazon.com>
2017-11-20 20:48:40 -0800

    Add a method to get all the cities in the api and inset them in a text file

--- src/main/java/com/amazon/asksdk/cloudfoody/OpenTable.java
+++ src/main/java/com/amazon/asksdk/cloudfoody/OpenTable.java
@@ -39,4 +39,24 @@ public class OpenTable {
         URLConnection connection = createConnection(fullURL);
         return new ObjectMapper().readValue(new InputStreamReader((connection.getInputStream())), HashMap.class);
     }
+
+    /*
+        The method creates a text file with all the available cities in the API
+        it also returns an ArrayList with all of those cities
+     */
+    public static ArrayList getAllCities() throws IOException {
+        String fullURL = BASE_URL + "cities";
+        URLConnection connection = createConnection(fullURL);
+        HashMap resultsHash = new ObjectMapper().readValue(new InputStreamReader((connection.getInputStream())), HashMap.class);
+        ArrayList cities = (ArrayList)resultsHash.get("cities");
+
+        FileWriter writer = new FileWriter("cities.txt");
+        for(Object city: cities) {
+            writer.write((String)city);
+            writer.write("\n");
+        }
+        writer.close();
+        System.out.println(cities);
+        return cities;
+    }
 }
