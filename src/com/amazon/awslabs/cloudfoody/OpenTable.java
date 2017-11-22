0dec5c2f35b611c9a6092559e77e174e066e0e4b
Andrey Pogorelyy <pogorely@amazon.com>
2017-11-21 22:56:26 -0800

    Refactor OpenTable
    1. Return a list of restaurants by name.
    2. Return first page of 100 restaurants by city.
    3. Create a helper method to parse restaurants from JSON response.

--- src/com/amazon/awslabs/cloudfoody/OpenTable.java
+++ src/com/amazon/awslabs/cloudfoody/OpenTable.java
@@ -1,85 +1,75 @@
 package com.amazon.awslabs.cloudfoody;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.FileWriter;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class OpenTable {
+    private static final Logger logger = LoggerFactory.getLogger(OpenTable.class);
 
     private static final String BASE_URL = "http://opentable.herokuapp.com/api/";
 
     /**
-     *  The method establishes a connection and returns a url object
+     * Get restaurants by name from Unofficial Open Table API.
+     * @param name restaurant name
+     * @return a list of restaurants
+     * @throws IOException
      */
-    public static URLConnection createConnection(String urlString) throws IOException {
+    public static ArrayList<Restaurant> getRestaurantsByName(final String name) throws IOException {
+        String fullURL = BASE_URL + "restaurants?name=" + name;
+
+        URLConnection connection = createConnection(fullURL);
+
+        return parseRestaurants(connection.getInputStream());
+    }
+
+    /**
+     * Get restaurants by city from Unofficial Open Table API. Limited to the first 100 results.
+     * @param city city name
+     * @return a list of restaurants
+     * @throws IOException
+     */
+    public static ArrayList getRestaurantsInCity(final String city) throws IOException {
+        String fullURL = BASE_URL + "restaurants?city=" + city + "&per_page=100";
+        URLConnection connection = createConnection(fullURL);
+
+        return parseRestaurants(connection.getInputStream());
+    }
+
+    /**
+     *  Establish a connection and return a url object.
+     */
+    private static URLConnection createConnection(String urlString) throws IOException {
         URL url = new URL(urlString);
         return url.openConnection();
     }
 
-    /**
-     *  The method takes a restaurant name as an arguement and returns back an instance of the Restaurant class
-     *  and sets all the fields as well
-     */
-    public static Restaurant getRestaurantByName(String name) throws IOException {
-        String fullURL = BASE_URL + "restaurants?name=" + name;
-        URLConnection connection = createConnection(fullURL);
-        HashMap resultsHash = new ObjectMapper().readValue(new InputStreamReader((connection.getInputStream())), HashMap.class);
-        ArrayList restaurants = (ArrayList)resultsHash.get("restaurants");
-        HashMap restaurantHash = (HashMap)restaurants.get(0);
-        Restaurant restaurant = new Restaurant();
-        restaurant.setName((String) restaurantHash.get("name"));
-        restaurant.setRestId((int) restaurantHash.get("id"));
-        restaurant.setAddress((String) restaurantHash.get("address"));
-        restaurant.setCity((String) restaurantHash.get("city"));
-        restaurant.setState((String) restaurantHash.get("state"));
-        restaurant.setArea((String) restaurantHash.get("area"));
-        restaurant.setPostalCode((String) restaurantHash.get("postal_code"));
-        restaurant.setCountry((String) restaurantHash.get("country"));
-        restaurant.setPhone((String) restaurantHash.get("phone"));
-        restaurant.setLat((double) restaurantHash.get("lat"));
-        restaurant.setLng((double) restaurantHash.get("lng"));
-        restaurant.setPrice((int) restaurantHash.get("price"));
-        restaurant.setReserveURL((String) restaurantHash.get("reserve_url"));
-        restaurant.setMobileReserveURL((String) restaurantHash.get("mobile_reserve_url"));
-        restaurant.setImageURL((String) restaurantHash.get("img_url"));
-        return restaurant;
-    }
+    private static ArrayList<Restaurant> parseRestaurants(final InputStream inputStream) throws IOException {
+        final InputStreamReader streamReader = new InputStreamReader(inputStream);
+        final ObjectMapper mapper = new ObjectMapper();
+        final JsonNode root = mapper.readTree(streamReader);
 
-    /**
-     *  The method takes a city name and the current page number as arguements then it
-     *  returns back an ArrayList with all the restaurant names in that city
-     */
-    public static ArrayList getRestaurantsInCity(String city, int page) throws IOException {
-        String fullURL = BASE_URL + "restaurants?city=" + city + "&page=" + page + "&per_page=100";
-        URLConnection connection = createConnection(fullURL);
-        HashMap resultsHash = new ObjectMapper().readValue(new InputStreamReader((connection.getInputStream())), HashMap.class);
-        ArrayList jsonRestaurants = (ArrayList)resultsHash.get("restaurants");
-        ArrayList restaurants = new ArrayList();
-        for (Object jsonRestaurant: jsonRestaurants) {
-            Restaurant restaurant = new Restaurant();
-            restaurant.setName((String) ((HashMap)jsonRestaurant).get("name"));
-            restaurant.setRestId((int) ((HashMap)jsonRestaurant).get("id"));
-            restaurant.setAddress((String) ((HashMap)jsonRestaurant).get("address"));
-            restaurant.setCity((String) ((HashMap)jsonRestaurant).get("city"));
-            restaurant.setState((String) ((HashMap)jsonRestaurant).get("state"));
-            restaurant.setArea((String) ((HashMap)jsonRestaurant).get("area"));
-            restaurant.setPostalCode((String) ((HashMap)jsonRestaurant).get("postal_code"));
-            restaurant.setCountry((String) ((HashMap)jsonRestaurant).get("country"));
-            restaurant.setPhone((String) ((HashMap)jsonRestaurant).get("phone"));
-            restaurant.setLat((double) ((HashMap)jsonRestaurant).get("lat"));
-            restaurant.setLng((double) ((HashMap)jsonRestaurant).get("lng"));
-            restaurant.setPrice((int) ((HashMap)jsonRestaurant).get("price"));
-            restaurant.setReserveURL((String) ((HashMap)jsonRestaurant).get("reserve_url"));
-            restaurant.setMobileReserveURL((String) ((HashMap)jsonRestaurant).get("mobile_reserve_url"));
-            restaurant.setImageURL((String) ((HashMap)jsonRestaurant).get("img_url"));
-            restaurants.add(restaurant);
+        final ArrayNode restaurantsNode = (ArrayNode) root.get("restaurants");
+        final ArrayList<Restaurant> restaurants = new ArrayList();
+        for (JsonNode restaurantNode : restaurantsNode) {
+            try {
+                restaurants.add(mapper.treeToValue(restaurantNode, Restaurant.class));
+            } catch (final JsonProcessingException exception) {
+                logger.warn(
+                        "Failed to parse restaurant object with exception: {}".format(exception.getLocalizedMessage()));
+            }
         }
-        System.out.println(restaurants);
+
         return restaurants;
     }
 }
