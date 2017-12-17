ecb162baf4a9fa8ead4e32cc8e0a8fa0748c9ac3
Mina Habib <minahabi@amazon.com>
2017-12-16 19:01:32 -0800

    Add methods to return a random restaurant object

--- src/com/amazon/awslabs/cloudfoody/OpenTable.java
+++ src/com/amazon/awslabs/cloudfoody/OpenTable.java
@@ -13,6 +13,9 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class OpenTable {
     private static final Logger logger = LoggerFactory.getLogger(OpenTable.class);
@@ -20,40 +23,101 @@ public class OpenTable {
     private static final String BASE_URL = "http://opentable.herokuapp.com/api/";
 
     /**
+     * Get a random restaurant from a list of restaurants
+     * @param city
+     * @param state
+     * @return random restaurant object
+     * @throws IOException
+     */
+    public static Restaurant getRandomRestaurant(final String city, final String state) throws IOException {
+        ArrayList<Restaurant> restaurants = getRestaurantsInCity(city, state);
+        Random myRandomizer = new Random();
+        return restaurants.get(myRandomizer.nextInt(restaurants.size()));
+    }
+
+    /**
+     * Get restaurants by city from Unofficial Open Table API.
+     * @param city
+     * @param state
+     * @return ArrayList of restaurants
+     * @throws IOException
+     */
+    public static ArrayList<Restaurant> getRestaurantsInCity(String city, final String state) throws IOException {
+        int pageNumber = getRandomPage(city, state);
+        String fullURL = BASE_URL + "restaurants?city=" + replaceSpacesWithPlus(city) + "&state=" + state + "&page=" + pageNumber + "&per_page=100";
+        URLConnection connection = createConnection(fullURL);
+
+        return parseRestaurants(connection.getInputStream());
+    }
+
+    /**
+     * Get a random page number for multiple pages results
+     * @param city
+     * @param state
+     * @return int pageNumber
+     * @throws IOException
+     */
+    private static int getRandomPage(String city, final String state) throws IOException {
+        String fullURL = BASE_URL + "restaurants?city=" + replaceSpacesWithPlus(city) + "&state=" + state;
+        URLConnection connection = createConnection(fullURL);
+        HashMap resultsHash = new ObjectMapper().readValue(new InputStreamReader(connection.getInputStream()), HashMap.class);
+
+        /* Get the total number of restaurants at this city then divide is be maximum number of restaurants per page with is 100
+           to get the number of pages.
+         */
+
+        int totalRestaurants = (int) resultsHash.get("total_entries");
+        int numberOfPages = (int) Math.ceil(totalRestaurants/100.00);
+
+        if (numberOfPages == 1) {
+            return 1;
+        } else {
+            return ThreadLocalRandom.current().nextInt(1, numberOfPages + 1);
+        }
+    }
+
+    /**
      * Get restaurants by name from Unofficial Open Table API.
      * @param name restaurant name
      * @return a list of restaurants
      * @throws IOException
      */
-    public static ArrayList<Restaurant> getRestaurantsByName(final String name) throws IOException {
-        String fullURL = BASE_URL + "restaurants?name=" + name;
-
+    public static ArrayList<Restaurant> getRestaurantsByName(String name) throws IOException {
+        String fullURL = BASE_URL + "restaurants?name=" + replaceSpacesWithPlus(name);
         URLConnection connection = createConnection(fullURL);
-
         return parseRestaurants(connection.getInputStream());
     }
 
     /**
-     * Get restaurants by city from Unofficial Open Table API. Limited to the first 100 results.
-     * @param city city name
-     * @return a list of restaurants
-     * @throws IOException
+     * Check if a string contains spaces and replace them with pluses
+     * @param originalString
+     * @return String
      */
-    public static ArrayList getRestaurantsInCity(final String city) throws IOException {
-        String fullURL = BASE_URL + "restaurants?city=" + city + "&per_page=100";
-        URLConnection connection = createConnection(fullURL);
-
-        return parseRestaurants(connection.getInputStream());
+    private static String replaceSpacesWithPlus(final String originalString) {
+        if (originalString.contains(" ")) {
+            return originalString.replace(" ", "+");
+        } else {
+            return originalString;
+        }
     }
 
     /**
-     *  Establish a connection and return a url object.
+     *  Establish a connection.
+     *  @param urlString
+     *  @return url object
+     *  @throws IOException
      */
-    private static URLConnection createConnection(String urlString) throws IOException {
+    private static URLConnection createConnection(final String urlString) throws IOException {
         URL url = new URL(urlString);
         return url.openConnection();
     }
 
+    /**
+     * Parse a list of restaurant objects from josn
+     * @param inputStream
+     * @return ArrayList of restaurants
+     * @throws IOException
+     */
     private static ArrayList<Restaurant> parseRestaurants(final InputStream inputStream) throws IOException {
         final InputStreamReader streamReader = new InputStreamReader(inputStream);
         final ObjectMapper mapper = new ObjectMapper();
